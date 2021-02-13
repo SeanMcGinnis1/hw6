63a6acc5b01d2c69dc59cdb7f56471e468c0a4f7
Sean McGinnis <sean.mcginnis@kellogg.northwestern.edu>
2021-02-13 13:43:20 -0600

    Final Commit

--- homework/movies.js
+++ homework/movies.js
@@ -17,7 +17,11 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   // movies. Write the contents of this array to the JavaScript
   // console to ensure you've got good data
   // ⬇️ ⬇️ ⬇️
-
+    let apiKey = `23a3109b4d61d65c03ba0b27129626dd`
+    response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
+    json = await response.json()
+    console.log(json)
+    let db = firebase.firestore()
   // ⬆️ ⬆️ ⬆️ 
   // End Step 1
   
@@ -33,6 +37,38 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   //   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
   // </div>
   // ⬇️ ⬇️ ⬇️
+  let movie = document.querySelector('.movies')
+  let querySnapshot = await db.collection('items').get()
+  let movies = querySnapshot.docs
+  // for (let i=0; i<movies.length; i++) {
+  //   let movie = movies[i].data()
+  //   console.log(movie.id)
+  // }
+
+
+  for (let i=0; i<json.results.length; i++) {
+    movie.insertAdjacentHTML('beforeend',`
+  
+  <div class="w-1/5 p-4 movie-${json.results[i].id}">
+     <img src="https://image.tmdb.org/t/p/w500/${json.results[i].backdrop_path}" class="w-full">
+     <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+  </div>
+  `)
+    for (j=0; j < querySnapshot.size; j++) {
+      console.log(movies[j].id)
+      console.log(json.results[i].id)
+      let movie = movies[j].data()
+      console.log(movie.id)
+      if (movie.id == json.results[i].id) {
+        document.querySelector(`.movie-${json.results[i].id}`).classList.add('opacity-20')
+      }
+    }
+  }
+
+
+ 
+
+  
 
   // ⬆️ ⬆️ ⬆️ 
   // End Step 2
@@ -48,6 +84,17 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   //   the movie is watched. Use .classList.remove('opacity-20')
   //   to remove the class if the element already contains it.
   // ⬇️ ⬇️ ⬇️
+
+  for (let i=0; i<json.results.length;i++) {
+    document.querySelector(`.movie-${json.results[i].id} .watched-button`).addEventListener('click', async function(event) {
+      event.preventDefault()
+      console.log(json.results[i].original_title)
+      document.querySelector(`.movie-${json.results[i].id}`).classList.add('opacity-20')
+
+      let docRef = await db.collection('items').add({
+        id: `${json.results[i].id}`
+      })
+  })}
 
   // ⬆️ ⬆️ ⬆️ 
   // End Step 3
